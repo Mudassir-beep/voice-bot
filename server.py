bc792b5c95c70065a243581aa64018693d71f894
Mudassir Shah <muddather.abbas@gmail.com>
2026-06-24 01:16:37 +0300

    Update server.py

--- server.py
+++ server.py
@@ -196,4 +196,4 @@ async def proxy_http(request: Request, path: str):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=PORT)
+   uvicorn.run("server:app", host="0.0.0.0", port=PORT)
